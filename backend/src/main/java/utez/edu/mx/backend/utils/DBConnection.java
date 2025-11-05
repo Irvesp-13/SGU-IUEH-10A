ed0deceb5cdf45dcc3ec407ce6271bfbe03c528c
Irivng Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2025-11-04 23:58:37 -0600

    [ADD] Ajustes en la base de datos y el backend

--- backend/src/main/java/utez/edu/mx/backend/utils/DBConnection.java
+++ backend/src/main/java/utez/edu/mx/backend/utils/DBConnection.java
@@ -6,31 +6,3 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-
-@Configuration
-public class DBConnection {
-    @Value("${db.host}")
-    private String host;
-
-    @Value("${db.port}")
-    private String port;
-
-    @Value("${db.name}")
-    private String name;
-
-    @Value("${db.user}")
-    private String user;
-
-    @Value("${db.pass}")
-    private String pass;
-
-    @Bean
-    public DataSource getDBConnection() {
-        DriverManagerDataSource source = new DriverManagerDataSource();
-        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        source.setUrl("jdbc:mysql://" + host + ":" + port + "/" + name);
-        source.setUsername(user);
-        source.setPassword(pass);
-        return source;
-    }
-}
